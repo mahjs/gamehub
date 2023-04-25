d18cf94d7430bce97a821f385227f1c5e0b607b2
mahjs <mahjsdev@gmail.com>
2023-04-25 22:07:32 +0330

    Implementing the aside column

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCropedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
   return (
-    <ul>
+    <List>
       {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="8px">
+          <HStack>
+            <Image
+              src={getCropedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
