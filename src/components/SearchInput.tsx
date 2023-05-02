db1b3c8fb400ffff47e9dc7192b64983d0e507f2
mahjs <mahjsdev@gmail.com>
2023-05-02 21:25:15 +0330

    Adding readme file

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,8 +2,10 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
+  const navigate = useNavigate();
   const searchRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameStore((store) => store.setSearchText);
   return (
@@ -11,6 +13,7 @@ const SearchInput = () => {
       onSubmit={(e) => {
         e.preventDefault();
         if (searchRef.current) setSearchText(searchRef.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
